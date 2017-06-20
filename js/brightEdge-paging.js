13e0ef46550cd3943a6b07940ce13dfd8b8797b7
Akshar <patelakshar.patel98@gmail.com>
2017-06-20 16:37:53 -0400

    needed to fix few bugs

--- js/brightEdge-paging.js
+++ js/brightEdge-paging.js
@@ -1,61 +1,50 @@
-var current_page = 1;
-var records_per_page = globCon.config.end;
-console.log(globCon.config);
 globCon.config.nextPage = nextPage;
 globCon.config.prevPage = prevPage;
-globCon.config.current_page = current_page;
-function prevPage()
-{
-    if (current_page > 1) {
-        current_page--;
-        changePage(current_page);
+globCon.config.changePage = changePage;
+globCon.config.numPages = numPages;
+globCon.config.current_page = 1;
+
+function prevPage() {
+    if (globCon.config.current_page > 1) {
+        globCon.config.current_page--;
+        globCon.config.changePage(globCon.config.current_page);
     }
 }
 
-function nextPage()
-{ 
-    if (current_page < numPages()) {
-        current_page++;
-        changePage(current_page);
+function nextPage() {
+    if (globCon.config.current_page < globCon.config.numPages()) {
+        globCon.config.current_page++;
+        globCon.config.changePage(globCon.config.current_page);
     }
 }
-    
-function changePage(page)
-{
+
+function changePage(page) {
     var btn_next = document.getElementById("btn_next");
     var btn_prev = document.getElementById("btn_prev");
-    // var listing_table = document.getElementById("listingTable");
     var page_span = document.getElementById("page");
- 
+    var records_per_page = globCon.config.end;
+    
     // Validate page
-    if (page < 1) page = 1;
-    if (page > numPages()) page = numPages();
+    if (page <= 1) page = 1;
+    if (page > globCon.config.numPages()) page = globCon.config.numPages();
 
-    // listing_table.innerHTML = "";
-    globCon.config.dynamicTable((page-1) * records_per_page, (page * records_per_page));
-    // for (var i = (page-1) * records_per_page; i < (page * records_per_page); i++) {
-    //     listing_table.innerHTML += objJson[i].adName + "<br>";
-    // }
+    globCon.config.dynamicTable((page - 1) * records_per_page, (page * records_per_page));
     page_span.innerHTML = page;
 
-    if (page == 1) {
+    if (page == 1 ) {
         btn_prev.style.visibility = "hidden";
     } else {
         btn_prev.style.visibility = "visible";
     }
 
-    if (page == numPages()) {
+    if (page == globCon.config.numPages()) {
         btn_next.style.visibility = "hidden";
     } else {
         btn_next.style.visibility = "visible";
     }
 }
 
-function numPages()
-{
-    return Math.ceil(globCon.config.rows.length / records_per_page);
+function numPages() {
+    // number of pages = next integer of (total data to display/ total data per page)
+    return Math.ceil(globCon.config.rows.length / globCon.config.end);
 }
-
-window.onload = function() {
-    changePage(1);
-};
--- js/brightEdge-script.js
+++ js/brightEdge-script.js
@@ -1,21 +1,17 @@
+// declared a global object to share data between different modules.
 var globCon = {};
 (function iife() {
-    var xhttp, current_page, totalOptions, records_per_page,
-        totalData, objJson, config, current_page,  records_per_page;
+    var xhttp, current_page, totalOptions, totalData, objJson, config, records_per_page;
     xhttp = new XMLHttpRequest();
-    current_page = 1;
-    records_per_page = 2;
     totalOptions = document.getElementById("optionSelected");
+    //whenever there is a change in selection of enteries per page, 
+    //rerender the dom according to number of enteries starting from first page
     totalOptions.addEventListener("change", optionChanged);
 
     globCon.config = {
-        pages: 10,
         start: 0,
         end: 2,
         dynamicTable: dynamicTable,
-        // nextPage: nextPage,
-        // prevPage: prevPage,
-        changePage: changePage,
         rows: null
     }
 
@@ -37,19 +33,16 @@ var globCon = {};
     }
 
     function addDatatoConfig(data) {
+        //store the data from server in rows property 
         globCon.config.rows = JSON.parse(data);
         pagination();
     }
 
     function dynamicTable(start, end) {
-        // var data = JSON.parse(dataTodisplay);
-        // objJson = data;
-        // totalData = data.length;
         var result = '';
         var value = globCon.config.rows;
-        var lindex = globCon.config.rows.length - 1;
-        if (end > lindex) {
-            end = lindex;
+        if (end > value.length-1) {
+            end = value.length-1;
         }
         for (var i = start; i < end; i++) {
             result += "<tr><td data-label='name'>" + value[i].name +
@@ -62,68 +55,10 @@ var globCon = {};
         document.getElementById("dataToDisplay").innerHTML = result;
     }
     fetchData();
-    // function prevPage() {
-    //         if (current_page > 1) {
-    //             current_page--;
-    //             changePage(current_page);
-    //         }
-    //     }
-
-    //     function nextPage() {
-    //         console.log("sdfjlkdsff");
-    //         if (current_page < numPages()) {
-    //             current_page++;
-    //             changePage(current_page);
-    //         }
-    //     }
-        function changePage(page) {
-            var btn_next = document.getElementById("btn_next");
-            var btn_prev = document.getElementById("btn_prev");
-            // var listing_table = document.getElementById("listingTable");
-            var page_span = document.getElementById("page");
-
-            // Validate page
-            if (page < 1) page = 1;
-            if (page > numPages()) page = numPages();
-
-            // listing_table.innerHTML = "";
-            globCon.config.dynamicTable((page - 1) * records_per_page, (page * records_per_page));
-            // for (var i = (page-1) * records_per_page; i < (page * records_per_page); i++) {
-            //     listing_table.innerHTML += objJson[i].adName + "<br>";
-            // }
-            page_span.innerHTML = page;
-
-            if (page == 1) {
-                btn_prev.style.visibility = "hidden";
-            } else {
-                btn_prev.style.visibility = "visible";
-            }
-
-            if (page == numPages()) {
-                btn_next.style.visibility = "hidden";
-            } else {
-                btn_next.style.visibility = "visible";
-            }
-        }
-
 
     function pagination() {
-        current_page = 1;
-        records_per_page = globCon.config.end;
-        console.log(globCon.config.end);
-
-        
-
-        
-        function numPages() {
-            return Math.ceil(globCon.config.rows.length / records_per_page);
-        }
-
-
-        dynamicTable(globCon.config.start, globCon.config.end);
+        globCon.config.current_page = 1;
+        globCon.config.changePage(globCon.config.current_page);
     }
-    window.onload = function() {
-    changePage(1);
-};
 
 }());
