878b50cc9fa508e0286661390e56c19ff3f93c4f
patela08 <patelakshar.patel98@gmail.com>
2017-06-20 23:53:00 -0400

    final Commit

--- js/brightEdge-paging.js
+++ js/brightEdge-paging.js
@@ -1,50 +1,52 @@
-globCon.config.nextPage = nextPage;
-globCon.config.prevPage = prevPage;
-globCon.config.changePage = changePage;
-globCon.config.numPages = numPages;
-globCon.config.current_page = 1;
+(function iife() {
+    globCon.config.nextPage = nextPage;
+    globCon.config.prevPage = prevPage;
+    globCon.config.changePage = changePage;
+    globCon.config.numPages = numPages;
+    globCon.config.current_page = 1;
 
-function prevPage() {
-    if (globCon.config.current_page > 1) {
-        globCon.config.current_page--;
-        globCon.config.changePage(globCon.config.current_page);
-    }
-}
-
-function nextPage() {
-    if (globCon.config.current_page < globCon.config.numPages()) {
-        globCon.config.current_page++;
-        globCon.config.changePage(globCon.config.current_page);
-    }
-}
-
-function changePage(page) {
-    var btn_next = document.getElementById("btn_next");
-    var btn_prev = document.getElementById("btn_prev");
-    var page_span = document.getElementById("page");
-    var records_per_page = globCon.config.end;
-    
-    // Validate page
-    if (page <= 1) page = 1;
-    if (page > globCon.config.numPages()) page = globCon.config.numPages();
-
-    globCon.config.dynamicTable((page - 1) * records_per_page, (page * records_per_page));
-    page_span.innerHTML = page;
-
-    if (page == 1 ) {
-        btn_prev.style.visibility = "hidden";
-    } else {
-        btn_prev.style.visibility = "visible";
+    function prevPage() {
+        if (globCon.config.current_page > 1) {
+            globCon.config.current_page--;
+            globCon.config.changePage(globCon.config.current_page);
+        }
     }
 
-    if (page == globCon.config.numPages()) {
-        btn_next.style.visibility = "hidden";
-    } else {
-        btn_next.style.visibility = "visible";
+    function nextPage() {
+        if (globCon.config.current_page < globCon.config.numPages()) {
+            globCon.config.current_page++;
+            globCon.config.changePage(globCon.config.current_page);
+        }
     }
-}
 
-function numPages() {
-    // number of pages = next integer of (total data to display/ total data per page)
-    return Math.ceil(globCon.config.rows.length / globCon.config.end);
-}
+    function changePage(page) {
+        var btn_next = document.getElementById("btn_next");
+        var btn_prev = document.getElementById("btn_prev");
+        var page_span = document.getElementById("page");
+        var records_per_page = globCon.config.end;
+
+        // Validate page
+        if (page <= 1) page = 1;
+        if (page > globCon.config.numPages()) page = globCon.config.numPages();
+
+        globCon.config.dynamicTable((page - 1) * records_per_page, (page * records_per_page));
+        page_span.innerHTML = page;
+
+        if (page == 1) {
+            btn_prev.style.visibility = "hidden";
+        } else {
+            btn_prev.style.visibility = "visible";
+        }
+
+        if (page == globCon.config.numPages()) {
+            btn_next.style.visibility = "hidden";
+        } else {
+            btn_next.style.visibility = "visible";
+        }
+    }
+
+    function numPages() {
+        // number of pages = next integer of (total data to display/ total data per page)
+        return Math.ceil(globCon.config.rows.length / globCon.config.end);
+    }
+})();
--- js/brightEdge-sorting.js
+++ js/brightEdge-sorting.js
@@ -1,50 +1,52 @@
-
-function sortTable(table, col, reverse) {
-    temp = col;
-    var tb = table.tBodies[0], // use `<tbody>` to ignore `<thead>` and `<tfoot>` rows
-        tr = Array.prototype.slice.call(tb.rows, 0), // put rows into array
-        i;
-    reverse = -((+reverse) || -1);
-    tr = tr.sort(function (a, b) { // sort rows
-        return reverse // `-1 *` if want opposite order
-            *
-            (a.cells[col].textContent.trim() // using `.textContent.trim()` for test
-                .localeCompare(b.cells[col].textContent.trim())
-            );
-    });
-    for (i = 0; i < tr.length; ++i) tb.appendChild(tr[i]); // append each row in order
-}
-var temp = 0;
-function makeSortable(table) {
-
-    var th = table.tHead,
-        i;
-    th && (th = th.rows[0]) && (th = th.cells);
-    if (th) i = th.length;
-    else return; // if no `<thead>` then do nothing
-    while (--i >= 0)(function (i) {
-        var dir = 1;
-        th[i].addEventListener('click', function () {
-            if(temp !== i){
-                document.querySelectorAll("th")[temp].className = "sort";
-            }
-            if (this.className == "sort" || this.className == "dsc") {
-                this.className = "asc";
-            } else {
-                this.className = "dsc";
-            }
-            sortTable(table, i, (dir = 1 - dir))
+(function iife() {
+    function sortTable(table, col, reverse) {
+        temp = col;
+        var tb = table.tBodies[0], // use `<tbody>` to ignore `<thead>` and `<tfoot>` rows
+            tr = Array.prototype.slice.call(tb.rows, 0), // put rows into array
+            i;
+        reverse = -((+reverse) || -1);
+        tr = tr.sort(function (a, b) { // sort rows
+            return reverse // `-1 *` if want opposite order
+                *
+                (a.cells[col].textContent.trim() // using `.textContent.trim()` for test
+                    .localeCompare(b.cells[col].textContent.trim())
+                );
         });
-    }(i));
-}
+        for (i = 0; i < tr.length; ++i) tb.appendChild(tr[i]); // append each row in order
+    }
+    var temp = 0;
 
-function makeAllSortable(parent) {
-    parent = parent || document.body;
-    var t = parent.getElementsByTagName('table'),
-        i = t.length;
-    while (--i >= 0) makeSortable(t[i]);
-}
+    function makeSortable(table) {
 
-window.onload = function () {
-    makeAllSortable();
-};
+        var th = table.tHead,
+            i;
+        th && (th = th.rows[0]) && (th = th.cells);
+        if (th) i = th.length;
+        else return; // if no `<thead>` then do nothing
+        while (--i >= 0)(function (i) {
+            var dir = 1;
+            th[i].addEventListener('click', function () {
+                if (temp !== i) {
+                    document.querySelectorAll("th")[temp].className = "sort";
+                }
+                if (this.className == "sort" || this.className == "dsc") {
+                    this.className = "asc";
+                } else {
+                    this.className = "dsc";
+                }
+                sortTable(table, i, (dir = 1 - dir))
+            });
+        }(i));
+    }
+
+    function makeAllSortable(parent) {
+        parent = parent || document.body;
+        var t = parent.getElementsByTagName('table'),
+            i = t.length;
+        while (--i >= 0) makeSortable(t[i]);
+    }
+
+    window.onload = function () {
+        makeAllSortable();
+    };
+})();
