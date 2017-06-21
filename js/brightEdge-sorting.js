3b598869e0329deab71c861978ab15d539341b8e
patela08 <patelakshar.patel98@gmail.com>
2017-06-20 20:38:11 -0400

    working asc dsc

--- js/brightEdge-sorting.js
+++ js/brightEdge-sorting.js
@@ -5,28 +5,61 @@ function sortTable(table, col, reverse) {
     reverse = -((+reverse) || -1);
     tr = tr.sort(function (a, b) { // sort rows
         return reverse // `-1 *` if want opposite order
-            * (a.cells[col].textContent.trim() // using `.textContent.trim()` for test
+            *
+            (a.cells[col].textContent.trim() // using `.textContent.trim()` for test
                 .localeCompare(b.cells[col].textContent.trim())
-               );
+            );
     });
-    for(i = 0; i < tr.length; ++i) tb.appendChild(tr[i]); // append each row in order
+    for (i = 0; i < tr.length; ++i) tb.appendChild(tr[i]); // append each row in order
 }
 
 function makeSortable(table) {
-    var th = table.tHead, i;
+
+    var th = table.tHead,
+        i;
     th && (th = th.rows[0]) && (th = th.cells);
     if (th) i = th.length;
     else return; // if no `<thead>` then do nothing
-    while (--i >= 0) (function (i) {
+    while (--i >= 0)(function (i) {
         var dir = 1;
-        th[i].addEventListener('click', function () {sortTable(table, i, (dir = 1 - dir))});
+        // var temp = 0;
+        th[i].addEventListener('click', function () {
+            if (this.className == "sort asd" || this.className == "dsc asd") {
+                this.className = "asc";         
+                // this.className  += " asd";
+            } else {
+                this.className = "dsc";
+                // this.className  += " asd";
+            }
+             
+            console.log(this.className);
+            var els = document.getElementsByClassName("asd");
+            var j = els.length;
+            while(j--){
+                console.log("sdf");
+                els[j].className = "sort asd";
+            }
+            this.className += " asd";
+            sortTable(table, i, (dir = 1 - dir))
+        });
+        console.log(document.getElementsByClassName("asd")[i]);
+        document.getElementsByClassName("asd")[i].className = "sort";
     }(i));
+    // document.getElementsByClassName("asd")[i].className = "sort";
 }
 
 function makeAllSortable(parent) {
     parent = parent || document.body;
-    var t = parent.getElementsByTagName('table'),i = t.length;
+    var t = parent.getElementsByTagName('table'),
+        i = t.length;
     while (--i >= 0) makeSortable(t[i]);
 }
 
-window.onload = function () {makeAllSortable();};
+// document.getElementsByClassName('sort')[0].addEventListener("click", function () {
+//     // this.style.
+//     console.log(this.style);
+// })
+
+window.onload = function () {
+    makeAllSortable();
+};
